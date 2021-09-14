df44a958c2ebf945312abbc1b7609fcfb92dfd78
Miguel <mikidam2afuera@gmail.com>
2021-09-14 12:47:46 +0200

    clase

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        function1();
-        System.out.println("Esta línea se ejecuta en tercer lugar");
         function3();
+        System.out.println("Esta línea se ejecuta en tercer lugar");
+        function2();
+        function1();
     }
 
     public static void function1(){
         System.out.println("Esta línea se ejecuta en primer lugar");
-        function2();
     }
 
     public static void function2(){
